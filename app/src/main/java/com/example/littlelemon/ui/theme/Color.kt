c28d487071d46fa169de80f88dd7ed4a3a203d83
Alejandro Monge <almartinez0707@gmail.com>
2023-02-09 13:35:26 -0600

    Updated Colors with Little Lemon's color palette.

--- app/src/main/java/com/example/littlelemon/ui/theme/Color.kt
+++ app/src/main/java/com/example/littlelemon/ui/theme/Color.kt
@@ -2,7 +2,9 @@ package com.example.littlelemon.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Primary_Green = Color(0xFF495E57)
+val Primary_Yellow = Color(0xFFF4CE14)
+val Secondary_Orange = Color(0xFFEE9972)
+val Secondary_Pink = Color(0xFFFBDABB)
+val Secondary_White = Color(0xFFEDEFEE)
+val Secondary_Black = Color(0xFF333333)
