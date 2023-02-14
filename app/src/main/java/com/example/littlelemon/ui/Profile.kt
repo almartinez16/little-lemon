60b4697c6da8632de041912c3ad35b3d58ae4d4c
Alejandro Monge <almartinez0707@gmail.com>
2023-02-14 16:35:49 -0600

    Created Profile file and code for navigation testing.

--- app/src/main/java/com/example/littlelemon/ui/Profile.kt
+++ app/src/main/java/com/example/littlelemon/ui/Profile.kt
@@ -1,2 +1,23 @@
 package com.example.littlelemon.ui
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun Profile() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(
+            text = "Profile",
+            fontSize = 64.sp
+        )
+    }
+}
