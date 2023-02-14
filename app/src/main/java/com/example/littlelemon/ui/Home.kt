435c0f7d6c1df5cd631920cb6e98451df7794d98
Alejandro Monge <almartinez0707@gmail.com>
2023-02-14 16:37:00 -0600

    Created Home file and added code for navigation testing.

--- app/src/main/java/com/example/littlelemon/ui/Home.kt
+++ app/src/main/java/com/example/littlelemon/ui/Home.kt
@@ -1,2 +1,26 @@
 package com.example.littlelemon.ui
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import com.example.littlelemon.ui.navigation.Profile
+
+@Composable
+fun Home(navController: NavHostController) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+            .clickable { navController.navigate(Profile.route) }
+    ) {
+        Text(
+            text = "Home",
+            fontSize = 64.sp
+        )
+    }
+}
