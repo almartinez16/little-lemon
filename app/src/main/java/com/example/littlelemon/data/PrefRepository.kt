7ad73bfd7afd860e9aea43fcbd2d0a25283395c8
Alejandro Monge <almartinez0707@gmail.com>
2023-02-14 16:30:35 -0600

    Created Shared Preferences class and defined functions for login status and save/obtain first name, last name, and email.

--- app/src/main/java/com/example/littlelemon/data/PrefRepository.kt
+++ app/src/main/java/com/example/littlelemon/data/PrefRepository.kt
@@ -1,9 +1,44 @@
 package com.example.littlelemon.data
 
 import android.content.Context
+import android.content.SharedPreferences
 
-class PreferencesRepository(
-    val context: Context
+class PrefRepository(
+    private val context: Context
 ) {
-    
+    private val pref: SharedPreferences = context.getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE)
+
+    private val editor = pref.edit()
+
+    fun saveLoggedIn(isLoggedIn: Boolean) {
+        editor.putBoolean(PREF_LOGGED_IN,isLoggedIn).apply()
+    }
+
+    fun getLoggedIn(): Boolean {
+        return pref.getBoolean(PREF_LOGGED_IN, false)
+    }
+
+    fun saveUserFirstName(firstName: String) {
+        editor.putString(PREF_USER_FIRST_NAME, firstName).apply()
+    }
+
+    fun getUserFirstName(): String {
+        return pref.getString(PREF_USER_FIRST_NAME, "")!!
+    }
+
+    fun saveUserLastName(lastName: String) {
+        editor.putString(PREF_USER_LAST_NAME, lastName).apply()
+    }
+
+    fun getUserLastName(): String {
+        return pref.getString(PREF_USER_LAST_NAME, "")!!
+    }
+
+    fun saveUserEmail(email: String) {
+        editor.putString(PREF_USER_EMAIL, email).apply()
+    }
+
+    fun getUserEmail(): String {
+        return pref.getString(PREF_USER_EMAIL, "")!!
+    }
 }
