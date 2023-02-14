65e145d5e1f5db40e0984652ce27f931bb9448ad
Alejandro Monge <almartinez0707@gmail.com>
2023-02-14 14:55:36 -0600

    Updated imports and package

--- app/src/main/java/com/example/littlelemon/ui/Onboarding.kt
+++ app/src/main/java/com/example/littlelemon/ui/Onboarding.kt
@@ -1,4 +1,4 @@
-package com.example.littlelemon
+package com.example.littlelemon.ui
 
 import android.content.Context
 import androidx.compose.foundation.Image
@@ -9,7 +9,6 @@ import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -18,6 +17,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.littlelemon.R
 import com.example.littlelemon.ui.theme.*
 import com.example.littlelemon.ui.toast
 
